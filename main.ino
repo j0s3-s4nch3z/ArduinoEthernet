e9eeaeb210b9ffcf5e872cb9ac1e82430376fba5
Jose Sanchez <jsanchez@adportas.cl>
2018-12-10 19:40:39 -0300

    first commit

---
+++ main.ino
@@ -0,0 +1,42 @@
+#include <Ethernet.h>
+#include <SPI.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte ip[] = { 192, 168, 100, 199 };
+byte server[] = { 192, 168, 100, 30}; 
+
+EthernetClient client;
+
+void setup()
+{
+  Ethernet.begin(mac, ip);
+  Serial.begin(9600);
+
+  delay(1000);
+
+  Serial.println("connecting...");
+
+  if (client.connect(server, 8080)) {
+    Serial.println("connected");
+    client.println("GET /Test/index.jsp?test=ok HTTP/1.0");
+    client.println();
+  } else {
+    Serial.println("connection failed");
+  }
+}
+
+void loop()
+{
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+    for(;;)
+      ;
+  }
+}
